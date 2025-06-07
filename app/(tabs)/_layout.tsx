2a26532e8095ad02cf613a4cdf687acbee675840
emperorbj <cwaku96@gmail.com>
2025-06-07 17:33:15 +0100

    setup the configurations for the project

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,43 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs} from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import COLORS from '@/constants/constants'
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const TabLayout = () => {
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+  const insets = useSafeAreaInsets() 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs screenOptions={{
+      headerShown:false,
+      tabBarActiveTintColor:COLORS.primary,
+
+      headerShadowVisible:false,
+      tabBarStyle:{
+        backgroundColor:COLORS.cardBackground,
+        borderTopColor:COLORS.border,
+        borderTopWidth:1,
+        paddingTop:5,
+        height:60 + insets.bottom,
+
+      }
+    }}>
+      <Tabs.Screen name="index" options={{
+        title:"Upload",
+        tabBarIcon:({color,size})=>(<Ionicons name="home-outline" size={size} color={color}/>)
+        }} /> 
+
+      <Tabs.Screen name="chat" options={{title:"Chat",
+        tabBarIcon:({color,size})=>(<Ionicons name="add-circle-outline" size={size} color={color}/>)
+      }}/> 
+      
+      <Tabs.Screen name="docs" options={{title:"Files",
+        tabBarIcon:({color,size})=>(<Ionicons name="person-outline" size={size} color={color}/>)
+      }}/>
+      
     </Tabs>
-  );
+  )
 }
+
+export default TabLayout
